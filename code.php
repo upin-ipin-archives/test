cf58c115309d12556f93af399000a0b06f6c1b5b
upin-ipin-archives <cfull062@gmail.com>
2025-01-28 12:13:22 +0700

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,3 @@
 <?php
-$url = 'https://google.com';
-$url = str_replace(array("[", "]", "{", "}"), "", $url);
-$content = shell_exec("curl " . escapeshellcmd($url));
-if ($content !== false) {
-    echo '<iframe srcdoc="' . htmlspecialchars($content) . '" class="white-box fixed-size-iframe" frameborder="0"></iframe>';
-} else {
-    echo '<img src="https://media.tenor.com/brIDwsDiqtQAAAAi/hololive.gif" alt="Hololive Agent" class="holo-gif">';
-}
+echo 'wow';
 ?>
